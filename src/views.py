8e14a8efdbc116386efaa7b79dfa27f185589df9
CuriousLearner <sanyam.khurana01@gmail.com>
2015-06-29 16:08:08 +0530

    API Updated with auto increment ID

--- src/views.py
+++ src/views.py
@@ -1,23 +1,26 @@
-from src import app
-from flask import render_template, jsonify, request, redirect, url_for, Response, make_response
+from src import app, db
+from flask import (render_template, jsonify, request, redirect, url_for, 
+                   Response, make_response)
 from models import Resource, Tags
 import json
 
 result = [] # Global result list to return result as JSON
+r = Resource() # Global Instance of Resource
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/resource/addresources', methods=['GET', 'POST'])
+@app.route('/api/resource/addresources/', methods=['GET', 'POST'])
 def add_resource():
     '''
     adds resources with POST request
     '''
-    r = Resource()
     if request.method == 'POST':
         if request.json:
-            r.rid = request.json['rid']
+            global r
+            r = Resource()
+            # r.rid = request.json['rid'] # Now automatically updated
             r.title = request.json['title']
             r.link = str(request.json['link'])
             # Insert Tags to Tags Object
@@ -27,11 +30,11 @@ def add_resource():
                 t.tag_name = item['tag_name']
                 r.tags.append(t)
             r.description = request.json['description']
-            Resource.save(r)
-            print r.__dict__
-    return Response(r)
+            r.save()
+    return Response(json.dumps(r, cls=PythonJSONEncoder), status=200, 
+                    content_type="application/json")
 
-@app.route('/api/resource/getresources')
+@app.route('/api/resource/getresources/')
 def get_resources():
     '''
     returns all the resources with GET request
@@ -42,23 +45,31 @@ def get_resources():
     return Response(json.dumps(result, cls=PythonJSONEncoder), status=200, 
                     content_type="application/json")
 
-@app.route('/api/resource/getresources/<int:resource_id>')
+@app.route('/api/resource/getresources/<int:resource_id>/')
 def update_or_delete_resource(resource_id):
     '''
-    Depending on the request, 
+    Depending on the request,
+    GET: Get the given resource
     PUT: Update the resource 
     or DELETE: Delete the resource
     '''
     global result
     allResources = Resource.objects.filter(rid=resource_id)
-    create_dict(allResources)
+    if allResources: # Resource is available in DB
+        if request.method == 'PUT': # Update the resource
+            pass
+        if request.method == 'DELETE': # Delete the resource
+            pass
+        create_dict(allResources)
+    else:
+        result = [{'Error': 'Resource not found'}]
     return Response(json.dumps(result, cls=PythonJSONEncoder), status=200, 
                     content_type="application/json")
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'Error': 'Not found'}), 404)
 
 
 class PythonJSONEncoder(json.JSONEncoder):
@@ -83,8 +94,8 @@ def unjsonify(dct):
 def create_dict(allResources):
     global result # To store the result of all resources
     result = [] # Empty for each call
-    d = {} # To make a dictionary for JSON Response
     for item in allResources:
+        d = {} # To make a dictionary for JSON Response
         d['rid'] = item.rid
         d['title'] = item.title
         d['link'] = item.link
